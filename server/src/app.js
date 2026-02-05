d773279a03294ecea48c915ad83cafdee7c9e85e
Abhimusmade010 <abhishekmusmade342@gmail.com>
2026-02-06 00:15:19 +0530

    Initial Setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,26 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
 
-const app=express();
+// import connectDB from "./config/db.js";
+// connectDB();
+
+const app = express();
+
+// Middlewares
+
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true
+// }));
+
 app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
+// app.use("/api", routes);
 
-app.get("/", (req, res) => res.send("Plan Stay API running"));
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
 
 export default app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,11 @@
-import app from './app.js'
+import app from "./app.js"
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const PORT=process.env.PORT || 3000;
+
+
+app.listen(PORT,()=>{
+    console.log(`Server listening on the port ${PORT}`);
+})
+
+
+
