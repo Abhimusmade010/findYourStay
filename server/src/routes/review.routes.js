3f0db229d8a15e75c850c1423a4f278313a1cd73
Abhimusmade010 <abhishekmusmade342@gmail.com>
2026-03-17 23:07:15 +0530

    Added validations for Review

--- server/src/routes/review.routes.js
+++ server/src/routes/review.routes.js
@@ -2,12 +2,13 @@ import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { getReviewsByHotelController } from "../controllers/review.controller.js";
 import { addReviewController } from "../controllers/review.controller.js";
-const router = express.Router();
+import { reviewSchemaforValidations } from "../validations/review.validations.js";
+import { validate } from "../middlewares/validate.middleware.js";
 
+const router = express.Router();
 
 router.get("/hotel/:hotelId", getReviewsByHotelController);
 
-
-router.post("/", authMiddleware, addReviewController);
+router.post("/", authMiddleware,validate(reviewSchemaforValidations),addReviewController);
 
 export default router;
