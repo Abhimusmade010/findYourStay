130f5a0a5ebe12b415b6112008015272535b86c5
Abhimusmade010 <abhishekmusmade342@gmail.com>
2026-03-28 10:43:16 +0530

    fixed the review vallidation

--- server/src/validations/review.validations.js
+++ server/src/validations/review.validations.js
@@ -5,9 +5,11 @@ export const reviewSchemaforValidations = z.object({
 
 //   hotel: z.string().regex(/^[a-f\d]{24}$/i, "Invalid hotel ID"),
 //   customer: z.string().regex(/^[a-f\d]{24}$/i, "Invalid customer ID"),
+
   rating: z.coerce.number().int().min(1).max(5),
   comment: z.string().trim().min(1).max(1000),
 
 });
+
 
 
