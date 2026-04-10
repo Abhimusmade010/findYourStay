ba9d3037eb634e9580a6e2cd9788c54a7d339c7c
Abhimusmade010 <abhishekmusmade342@gmail.com>
2026-04-10 19:05:36 +0530

    Added Helmt and Rate Limiter for security reasons,global for all routes and separate limiter for auth

--- server/app.js
+++ server/app.js
@@ -4,13 +4,18 @@ import cors from "cors"
 dotenv.config();
 import { swaggerSpec } from "./src/config/swagger.js";
 import swaggerUi from "swagger-ui-express";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+// Security middlewares
+
+
 
 import routes from './src/routes/index.js'
 const app = express();
 app.use(express.json());
+app.use(helmet());
 
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(cors({
   origin: [
@@ -19,6 +24,32 @@ app.use(cors({
   ],
   credentials: true
 }));
+
+// order of cors helmet and rate limit is as:
+// 1. CORS to handle cross-origin requests and preflight checks
+// 2. Helmet to set secure HTTP headers
+// 3. Rate limiting to prevent abuse after security checks are in place
+
+
+//  Strict limiter for auth
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20,
+  message: { status: "error", message: "Too many login attempts" }
+});
+
+//  General limiter for API
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
+app.use('/api/auth', authLimiter);
+app.use(globalLimiter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 
 
 app.use(express.urlencoded({extended:true}));
